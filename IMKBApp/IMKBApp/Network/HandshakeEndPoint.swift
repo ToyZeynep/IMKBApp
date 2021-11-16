976459958e95f12bff56751378b9bd60d99dd420
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2021-11-16 11:17:26 +0300

    Created Api EndPoints

--- IMKBApp/IMKBApp/Network/HandshakeEndPoint.swift
+++ IMKBApp/IMKBApp/Network/HandshakeEndPoint.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import Alamofire
+
+enum HandshakeEndPoint: APIConfiguration {
+    
+    case token(params: [String: Any])
+   
+    
+    var tokenUrl : String {
+    return "api/handshake/"
+    }
+    
+
+        var method: HTTPMethod {
+            switch self {
+            case .token:
+                return .post
+           
+            }
+        }
+    
+    
+    var path: String {
+        switch self {
+        case .token:
+            return tokenUrl + "start"
+       
+        }
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .token(let params):
+            return params
+        
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        
+        let baseUrl = try "https://mobilechallenge.veripark.com/".asURL()
+        
+        var urlRequest = URLRequest(url: baseUrl.appendingPathComponent(path))
+        
+        // HTTP Method
+        urlRequest.httpMethod = method.rawValue
+        
+        
+        // Parameters
+        let encoding: ParameterEncoding = {
+            switch method {
+            case .post:
+                return JSONEncoding.default
+            default:
+                return URLEncoding.default
+            }
+        }()
+        print(urlRequest)
+        return try encoding.encode(urlRequest, with: parameters)
+        print(urlRequest)
+        
+    }
+}
--- IMKBApp/IMKBApp/Network/StocksEndPoint.swift
+++ IMKBApp/IMKBApp/Network/StocksEndPoint.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import Alamofire
+
+enum StocksEndPoint: APIConfiguration {
+    
+    case stockList(params: [String: Any])
+    case stockDetails(params: [String: Any])
+    
+    var stockUrl : String {
+    return "api/stocks/"
+    }
+    
+
+        var method: HTTPMethod {
+            switch self {
+            case .stockList:
+                return .post
+            case .stockDetails:
+                return .post
+            }
+        }
+    
+    
+    var path: String {
+        switch self {
+        case .stockList:
+            return stockUrl + "list"
+        case .stockDetails:
+            return stockUrl + "detail"
+        }
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .stockList(let params):
+            return params
+        case .stockDetails(let params):
+            return params
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        
+        let baseUrl = try "https://mobilechallenge.veripark.com/".asURL()
+        
+        var urlRequest = URLRequest(url: baseUrl.appendingPathComponent(path))
+        
+        // HTTP Method
+        urlRequest.httpMethod = method.rawValue
+        
+        
+        // Parameters
+        let encoding: ParameterEncoding = {
+            switch method {
+            case .post:
+                return JSONEncoding.default
+            default:
+                return URLEncoding.default
+            }
+        }()
+        print(urlRequest)
+        return try encoding.encode(urlRequest, with: parameters)
+        print(urlRequest)
+        
+    }
+}
