801f6397c1c4e488fca8072395b951c311bf9fd5
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2021-11-16 09:06:50 +0300

    IOS-1 Created Project

---
+++ IMKBApp/IMKBApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMKBApp
+//
+//  Created by MacOS on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
