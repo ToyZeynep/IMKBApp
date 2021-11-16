a2bdff765764b0d7a65130a53274946c5e2f1ac4
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2021-11-16 10:58:00 +0300

    IOS-5 Edited Api Configuration File

--- IMKBApp/IMKBApp/Network/ApiConfiguration.swift
+++ IMKBApp/IMKBApp/Network/ApiConfiguration.swift
@@ -4,5 +4,11 @@
 //
 //  Created by MacOS on 16.11.2021.
 //
-
 import Foundation
+import Alamofire
+
+protocol APIConfiguration: URLRequestConvertible {
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+}
